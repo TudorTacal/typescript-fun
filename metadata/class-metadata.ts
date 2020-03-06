49ff4005d1a78d871337e3a5d76c0ffc17056f02
TudorTacal <tudortacal@gmail.com>
2020-03-06 08:52:06 +0000

    start metadata

---
+++ metadata/class-metadata.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+
+class Plane {
+  color: string = 'red';
+
+  fly(): void {
+    console.log('vrrrrr');
+  }
+}
--- metadata/metadata.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("reflect-metadata");
-var plane = {
-    color: 'red'
-};
-Reflect.defineMetadata('note', 'hi there', plane);
-var note = Reflect.getMetadata('note', plane);
-console.log(note);
